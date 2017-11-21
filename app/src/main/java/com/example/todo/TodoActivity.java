699ea8e997cae866d6bf99a3ac82d5d8997147bf
LBU-Levi <misternightmare@gmail.com>
2017-11-21 20:37:23 +0000

    Remove unused package imports.

--- app/src/main/java/com/example/todo/TodoActivity.java
+++ app/src/main/java/com/example/todo/TodoActivity.java
@@ -1,13 +1,10 @@
 
 package com.example.todo;
 
-import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
