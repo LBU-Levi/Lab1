0ed1c1c355b4b17deaa1a8a8d0c770f79f85dda7
LBU-Levi <misternightmare@gmail.com>
2017-11-21 21:03:04 +0000

    Changed colour of texts.

--- app/src/main/java/com/example/todo/TodoActivity.java
+++ app/src/main/java/com/example/todo/TodoActivity.java
@@ -8,14 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class TodoActivity extends AppCompatActivity {
+public class TodoActivity extends AppCompatActivity
+{
+    //Used to store our texts in res/values/strings.xml
+    private String[] m_stringArray;
 
-    private String[] mTodos;
-    private int mTodoIndex = 0;
+    //Used to store our colours assigned to the texts
+    private int[] m_colourArray;
+
+    //Stores index into m_stringArray for current text displayed on-screen
+    private int m_currentStringIndex = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState)
+    {
         // call the super class onCreate to complete the creation of activity like
         // the view hierarchy
         super.onCreate(savedInstanceState);
@@ -29,28 +35,43 @@ public class TodoActivity extends AppCompatActivity {
         TodoTextView = (TextView) findViewById(R.id.textViewTodo);
 
         // read the todo array from res/values/strings.xml
-        Resources res = getResources();
-        mTodos = res.getStringArray(R.array.todo);
+        m_stringArray = getResources().getStringArray(R.array.colour_names);
+
+
+        // Read up the colours array from res/values/colors.xml
+        m_colourArray = getResources().getIntArray(R.array.colour_values);
+
+
         // display the first task from mTodo array in the TodoTextView
-        TodoTextView.setText(mTodos[mTodoIndex]);
+        TodoTextView.setText(m_stringArray[m_currentStringIndex]);
+
+        //Set colour
+        TodoTextView.setTextColor(m_colourArray[m_currentStringIndex]);
 
         Button buttonNext, buttonPrev;
         buttonNext = (Button) findViewById(R.id.buttonNext);
         buttonPrev = (Button) findViewById(R.id.buttonPrev);
 
         // OnClick listener for the  Next button
-        buttonNext.setOnClickListener(new View.OnClickListener(){
+        buttonNext.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                TodoTextView.setText(mTodos[mTodoIndex = ++mTodoIndex % mTodos.length]);
+            public void onClick(View v)
+            {
+                TodoTextView.setText(m_stringArray[m_currentStringIndex = ++m_currentStringIndex % m_stringArray.length]);
+                TodoTextView.setTextColor(m_colourArray[m_currentStringIndex]);
+
             }
         });
 
         // OnClick listener for the  Prev button
-        buttonPrev.setOnClickListener(new View.OnClickListener(){
+        buttonPrev.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                TodoTextView.setText(mTodos[mTodoIndex == 0 ? mTodoIndex = mTodos.length-1 : --mTodoIndex]);
+            public void onClick(View v)
+            {
+                TodoTextView.setText(m_stringArray[m_currentStringIndex == 0 ? m_currentStringIndex = m_stringArray.length-1 : --m_currentStringIndex]);
+                TodoTextView.setTextColor(m_colourArray[m_currentStringIndex]);
             }
         });
     }
