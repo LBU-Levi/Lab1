8ec92e32a8cad5be793cda3b674dc070a8df1fd1
LBU-Levi <misternightmare@gmail.com>
2017-11-21 20:33:27 +0000

    Completed Lab1 Exercise.
    With working previous button.

---
+++ app/src/main/java/com/example/todo/TodoActivity.java
@@ -0,0 +1,60 @@
+
+package com.example.todo;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class TodoActivity extends AppCompatActivity {
+
+    private String[] mTodos;
+    private int mTodoIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // call the super class onCreate to complete the creation of activity like
+        // the view hierarchy
+        super.onCreate(savedInstanceState);
+
+        // set the user interface layout for this Activity
+        // the layout file is defined in the project res/layout/activity_todo.xml file
+        setContentView(R.layout.activity_todo);
+
+        // initialize member TextView so we can manipulate it later
+        final TextView TodoTextView;
+        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+
+        // read the todo array from res/values/strings.xml
+        Resources res = getResources();
+        mTodos = res.getStringArray(R.array.todo);
+        // display the first task from mTodo array in the TodoTextView
+        TodoTextView.setText(mTodos[mTodoIndex]);
+
+        Button buttonNext, buttonPrev;
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+        buttonPrev = (Button) findViewById(R.id.buttonPrev);
+
+        // OnClick listener for the  Next button
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                TodoTextView.setText(mTodos[mTodoIndex = ++mTodoIndex % mTodos.length]);
+            }
+        });
+
+        // OnClick listener for the  Prev button
+        buttonPrev.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                TodoTextView.setText(mTodos[mTodoIndex == 0 ? mTodoIndex = mTodos.length-1 : --mTodoIndex]);
+            }
+        });
+    }
+}
