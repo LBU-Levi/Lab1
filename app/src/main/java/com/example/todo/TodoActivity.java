0a139a181f790bff570d6d1ee8fea9edf7907ba3
LBU-Levi <misternightmare@gmail.com>
2017-11-22 02:30:26 +0000

    Add new activity

--- app/src/main/java/com/example/todo/TodoActivity.java
+++ app/src/main/java/com/example/todo/TodoActivity.java
@@ -1,13 +1,22 @@
 package com.example.todo;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TodoActivity extends AppCompatActivity
 {
+    private static final int IS_SUCCESS = 0;
+
     //Used to store/access our texts defined in res/values/strings.xml
     private String[] m_stringArray;
 
@@ -18,6 +27,7 @@ public class TodoActivity extends AppCompatActivity
     private int m_currentStringIndex = 0;
 
     private static final String STRING_INDEX_KEY = "STRING_INDEX";
+    private static final String IS_TODO_COMPLETE = "com.example.isTodoComplete";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -37,6 +47,56 @@ public class TodoActivity extends AppCompatActivity
 
         //Sets up default values and button listeners.
         InitialiseColoursApp();
+
+
+        Intent intent = TodoDetailActivity.newIntent(TodoActivity.this, m_currentStringIndex);
+        startActivity(intent);
+
+    }
+
+    /*
+    requestCode is the integer request code originally supplied to startActivityForResult
+    resultCode is the integer result code returned by the child activity through its setResult()
+    intent date attached with intent "extras"
+*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == IS_SUCCESS ){
+            if (intent != null) {
+                // data in intent from child activity
+                boolean isTodoComplete = intent.getBooleanExtra(IS_TODO_COMPLETE, false);
+                updateTodoComplete(isTodoComplete);
+            } else {
+                Toast.makeText(this, R.string.back_button_pressed, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, R.string.request_code_mismatch,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void updateTodoComplete(boolean is_todo_complete) {
+
+        final TextView textViewTodo;
+        textViewTodo = (TextView) findViewById(R.id.textViewTodo);
+
+        if (is_todo_complete) {
+            textViewTodo.setBackgroundColor(
+                    ContextCompat.getColor(this, R.color.backgroundSuccess));
+            textViewTodo.setTextColor(
+                    ContextCompat.getColor(this, R.color.colorSuccess));
+
+            setTextViewComplete("\u2713");
+        }
+    }
+
+    private void setTextViewComplete( String message ){
+        final TextView textViewComplete;
+        textViewComplete = (TextView) findViewById(R.id.textViewComplete);
+
+        textViewComplete.setText(message);
     }
 
     private void InitialiseColoursApp()
---
+++ app/src/main/java/com/example/todo/TodoDetailActivity.java
@@ -0,0 +1,56 @@
+package com.example.todo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TodoDetailActivity extends AppCompatActivity {
+
+    private static final String TODO_INDEX = "com.example.todoIndex";
+    private static final String IS_TODO_COMPLETE = "com.example.isTodoComplete";
+
+    public static Intent newIntent(Context packageContext, int todoIndex){
+        Intent intent = new Intent(packageContext, TodoDetailActivity.class);
+        intent.putExtra(TODO_INDEX,todoIndex);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_detail);
+        int todoIndex = getIntent().getIntExtra(TODO_INDEX, 0);
+
+        CheckBox checkboxIsComplete = (CheckBox)findViewById(R.id.checkBoxIsComplete);
+        // Register the onClick listener with the generic implementation mTodoListener
+        checkboxIsComplete.setOnClickListener(mTodoListener);
+
+    }
+
+    // Create an anonymous implementation of OnClickListener for all clickable view objects
+    private OnClickListener mTodoListener = new OnClickListener() {
+        public void onClick(View v) {
+            // get the clicked object and do something
+            switch (v.getId() /*to get clicked view object id**/) {
+                case R.id.checkBoxIsComplete:
+                    CheckBox checkboxIsComplete = (CheckBox) findViewById(R.id.checkBoxIsComplete);
+
+                    Intent intent = new Intent();
+                    intent.putExtra(IS_TODO_COMPLETE, checkboxIsComplete.isChecked());
+                    setResult(RESULT_OK, intent);
+                    finish();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+}
